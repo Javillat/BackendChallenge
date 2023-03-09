f42252472ffa750153433f56f2f5153f32ad67cd
Javier Villatoro <wemakesoftx@gmail.com>
2023-03-09 16:51:04 -0600

    chore: comments

--- src/controllers/userfn.js
+++ src/controllers/userfn.js
@@ -9,13 +9,15 @@ const bcrypt = require('bcrypt');
 postUser = async(req, res) => {
     try {
         const { email, name, password } = req.body;
-
+        //Realizar una busqueda interna del email que nos llega por body, si se encuenta, retorna un status 304 y un mensaje de no creado.
         const findone = await User.findOne({
             where:{email:{[Op.eq]:email}}
         });
-        console.log(findone);
+        //De no encontrar el email en la base de datos procede a crear el usuario con la informacion de body.
         if(!findone){
+            //Hacer un hash de la password encriptandola.
             const hashedPassword = await bcrypt.hash(password, 10)
+            //Registrar el usuario con los datos basicos.
             const newUser = await User.create({
                 email,
                 name,
