3c275ec15c746b382cdfb380a09c70bdf30d5530
Javier Villatoro <wemakesoftx@gmail.com>
2023-03-09 16:52:13 -0600

    chore: agregar ruta de productos

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 const { Router } = require('express');
 //Importar las rutas
 const routesuser = require('./routesuser.js');
+const routerproduct = require('./routerproduct.js');
 
 //Setear la ruta principal
 const routemain = Router();
 
 //Configurar la ruta media
 routemain.use('/user', routesuser);
+routemain.use('/product', routerproduct);
 
 //Exportar la ruta principal
 module.exports = routemain;
