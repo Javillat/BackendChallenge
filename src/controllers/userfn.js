09a9ad542f947c2a46889d0b21a1a26854fb9b6e
Javier Villatoro <wemakesoftx@gmail.com>
2023-04-14 22:48:16 -0600

    chore: agregar configuracion token

--- src/controllers/userfn.js
+++ src/controllers/userfn.js
@@ -1,6 +1,10 @@
+require("dotenv").config();
 const { User } = require('../db');
 const { Op } = require('sequelize');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+
 
 /**
  * POST... Registrar los usuarios dentro de la bd.
@@ -9,10 +13,10 @@ const bcrypt = require('bcrypt');
 postUser = async(req, res) => {
     try {
         const { email, name, password } = req.body;
-        //Realizar una rutina de comprovacion de campos vacios en el body
-        /**
-         * Aqui
-         */
+        //Realizar una rutina de comprovacion de campos vacios en el body...Lista
+        if (!(email, name, password)) {
+            res.status(400).send("Todos los campos son requeridos");
+        };
         
         //Realizar una busqueda interna del email que nos llega por body, si se encuenta, retorna un status 304 y un mensaje de no creado.
         const findone = await User.findOne({
@@ -24,17 +28,29 @@ postUser = async(req, res) => {
             const hashedPassword = await bcrypt.hash(password, 10)
             //Registrar el usuario con los datos basicos.
             const newUser = await User.create({
-                email,
+                email:email.toLowerCase(),
                 name,
                 password:hashedPassword,
             });
+
+        //Crear token.
+        const token = jwt.sign(
+            { user_id: newUser._id, email },
+            process.env.TOKEN_KEY,
+            {
+                expiresIn:"1h",
+            }
+        );
+        //Guardar el token.
+        newUser.token = token;
             return res.status(201).json(newUser);
         }else{
-            return res.status(304).send('Error,el usuario ya existe en nuestra bd, no creado')
+            return res.status(409).send('El usuario ya existe en nuestra bd. Por favor loguearse')
         }
     } catch (error) {
         console.error(error);
-        res.status(500).json({message: 'Error al crear el usuario'}); 
+        res.status(500).json({message: 'Error interno del servidor al crear el usuario'}); 
+        process.exit(1);
     }
 };
 
